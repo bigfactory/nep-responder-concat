c695bb573aaf76911ff4d1ac897861f12f0dd0f8
修名 <xiaocong.hxc@alibaba-inc.com>
2014-12-26 17:29:06 +0800

    init

---
+++ index.js
@@ -0,0 +1,34 @@
+var fs = require('fs');
+var path = require('path');
+var mime = require('mime');
+var async = require('async');
+var glob = require('glob');
+
+
+module.exports = function(req, res, next, data) {
+    var options = data.options;
+    var pattern = data.pattern;
+    var log = data.log;
+
+    var files = options.files;
+    var base = options.base||'';
+
+    var charset = options.charset || 'utf-8';
+    var localFiles = [];
+    var buffer = [];
+
+    files.forEach(function(file){
+        var res = glob.sync(path.join(base, file));
+        localFiles = localFiles.concat(res);
+    });
+
+    
+    localFiles.forEach(function(file){
+        var content = fs.readFileSync(file);
+        buffer.push(content);
+    });
+
+    buffer = buffer.join('');
+    res.setHeader('Content-Type', mime.lookup(localFiles[0])+'; charset='+charset);
+    res.send(buffer);
+};
